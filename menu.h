7e018ee00aab90ccac8249b384ec720e2342d101
Carlos Benito Jareño <39310173+carlw0S@users.noreply.github.com>
2024-08-09 18:37:03 +0200

    Make library header-only
    Even easier to use now, it should have been like this from the start 🥵

--- menu.h
+++ menu.h
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <vector>
+#include <sstream>
 
 /*
  * This function prints a simple menu and lets you select its options, 
@@ -15,4 +16,33 @@
  * @returns true if one of the options was executed, false if not.
 */
 bool menu(const std::string &title,
-          const std::vector< std::pair< std::string, bool (*)() > > &options);
+          const std::vector< std::pair< std::string, bool (*)() > > &options)
+{
+    bool end = false;
+    while (!end) {
+        // Print title and option names
+        std::cout << title << std::endl;
+        size_t i = 1;
+        for (const auto &option : options) {
+            std::cout << "    " << i << ") " << option.first << std::endl;
+            ++i;
+        }
+        std::cout << "    Enter anything else to cancel." << std::endl;
+
+        // Get selection from user input
+        size_t selection = 0;
+        std::string input_str;
+        std::getline(std::cin, input_str);
+        std::stringstream(input_str) >> selection;
+
+        if (selection != 0 && selection <= options.size()) {
+            // Option selected -- the menu will be shown again depending on its return value
+            end = options[selection - 1].second();
+        } else {
+            // Go back
+            return false;
+        }
+    }
+
+    return true;
+}
